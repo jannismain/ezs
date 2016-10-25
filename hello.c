cc5d1dd32c5f446b834fc1754a1414bfa5a63c0c
Jannis Mainczyk <ze25jelu@stud.informatik.uni-erlangen.de>
2016-10-25 17:29:13 +0200

    implemented sinf() for viewing on oszilloscop

--- hello.c
+++ hello.c
@@ -16,18 +16,31 @@
 
 /* Thread-Stack */
 #define STACKSIZE CYGNUM_HAL_STACK_SIZE_MINIMUM+1024
+#define PRIORITY 10
+
 static cyg_uint8 my_stack[STACKSIZE];
 static cyg_handle_t handle;
 static cyg_thread threaddata;
 
 void test_thread(cyg_addrword_t arg)
 {
-	ezs_printf("Hallo Welt!\n");
 	/* Einmalige Aufgaben ... */
+
+	float y_0 = 100;
+	float f = 10;
+	float omega = 2 * M_PI * f;
+	float offset = y_0;
+	float t = 0;
 
 	while(1)
 	{
 		/* Periodische Aufgaben ... */
+        float y = y_0 * sinf(omega * t) + offset;
+        ezs_dac_write((uint8_t) y);
+        // ezs_printf("Time: %f, Value: %f\n", t, y);
+        ezs_delay_us(10*1000);
+        t += 0.01;
+
 
 	}
 
@@ -41,8 +54,11 @@ void cyg_user_start(void)
 	ezs_counter_init();
 
 	/* Thread erzeugen ... */
-
+    cyg_thread_create(PRIORITY, &test_thread, 0, "mythread", my_stack, STACKSIZE, &handle, &threaddata);
 
 	/* Thread starten ... */
+	cyg_thread_resume(handle);
+
+    return;
 }
 
