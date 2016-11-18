204fd93ee8067602580713288f4ca6a0611bd459
Jannis Mainczyk <ze25jelu@stud.informatik.uni-erlangen.de>
2016-11-18 12:44:44 +0100

    added ex3 dir

--- Exercise2/libEZS/src/ezs_io.c
+++
@@ -1,8 +0,0 @@
-#include "ezs_io.h"
-#include <stdio.h>
-#include "ezs_counter.h"
-
-void ezs_print_measurement(cyg_uint32 timestamp, cyg_uint32 value) {
-    printf("time: %u value: %u\n", (unsigned int)timestamp, (unsigned int)value);
-}
-
