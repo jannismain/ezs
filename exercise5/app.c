8b9f2b7ca4f6e0cafdacf6796396f8a1dc982e95
helene <helene.gsaenger@fau.de>
2017-01-11 13:48:17 +0100

    changed some code

--- exercise5/app.c
+++ exercise5/app.c
@@ -26,6 +26,7 @@ static cyg_handle_t counter;
 volatile cyg_uint32 acc_t1;
 volatile cyg_uint32 acc_t2_a;
 volatile cyg_uint32 acc_t2_b;
+volatile cyg_uint32 acc_t2_c;
 volatile cyg_uint32 acc_t3;
 volatile cyg_uint32 acc_t4;
 
@@ -64,7 +65,7 @@ void task_1()
 void task_2_a()
 {
     ezs_dac_write(20);
-	cyg_uint32 ms = 5;
+	cyg_uint32 ms = 4;
 	cyg_uint32 ticks = ms_to_ezs_ticks(ms);
 	ezs_lose_time(ticks, 100);
     ezs_dac_write(0);
@@ -73,7 +74,16 @@ void task_2_a()
 void task_2_b()
 {
     ezs_dac_write(30);
-	cyg_uint32 ms = 5;
+	cyg_uint32 ms = 2;
+	cyg_uint32 ticks = ms_to_ezs_ticks(ms);
+	ezs_lose_time(ticks, 100);
+    ezs_dac_write(0);
+}
+
+void task_2_c()
+{
+    ezs_dac_write(40);
+	cyg_uint32 ms = 4;
 	cyg_uint32 ticks = ms_to_ezs_ticks(ms);
 	ezs_lose_time(ticks, 100);
     ezs_dac_write(0);
@@ -81,7 +91,7 @@ void task_2_b()
 
 void task_3()
 {
-    ezs_dac_write(40);
+    ezs_dac_write(50);
 	cyg_uint32 ms = 3;
 	cyg_uint32 ticks = ms_to_ezs_ticks(ms);
 	ezs_lose_time(ticks, 100);
@@ -89,7 +99,7 @@ void task_3()
 }
 void task_4()
 {
-    ezs_dac_write(50);
+    ezs_dac_write(60);
 	cyg_uint32 ms = 2;
 	cyg_uint32 ticks = ms_to_ezs_ticks(ms);
 	ezs_lose_time(ticks, 100);
@@ -99,47 +109,71 @@ void task_4()
 void thread(cyg_addrword_t data) {
     while(1) {
         if (acc_t4) {
-            acc_t4 = false;
             task_4();
-        }
-        else if (acc_t1) {
-            acc_t1 = false;
+            acc_t4 = false;
+        } else if (acc_t1) {
             task_1();
-        }
-        else if (acc_t3) {
-            acc_t3 = false;
+            acc_t1 = false;
+        } else if (acc_t3) {
             task_3();
-        }
-        else if (acc_t2_a) {
-            acc_t2_a = false;
+            acc_t3 = false;
+        } else if (acc_t2_a) {
             task_2_a();
-        }
-        else if (acc_t2_b) {
-            acc_t2_b = false;
+            acc_t2_a = false;
+        } else if (acc_t2_b) {
             task_2_b();
+            acc_t2_b = false;
+        } else if (acc_t2_c) {
+            task_2_c();
+            acc_t2_c = false;
         }
         cyg_thread_suspend(threadhndl1);        
     }
 }
 
+void check_deadlines()
+{
+    if (acc_t1) ezs_printf("Task 1 exceeded deadline\n");
+    if (acc_t2_a) ezs_printf("Task 2_a exceeded deadline\n");
+    if (acc_t2_b) ezs_printf("Task 2_b exceeded deadline\n");
+    if (acc_t2_c) ezs_printf("Task 2_c exceeded deadline\n");
+    if (acc_t3) ezs_printf("Task 3 exceeded deadline\n");
+    if (acc_t4) ezs_printf("Task 4 exceeded deadline\n");
+}
+
 void alarm_handler(cyg_handle_t alarm, cyg_addrword_t data) 
 {
     timer++;
+    //if (timer % 10 == 9)
+    //    check_deadlines();
+
     if (timer % 10 == 0) acc_t4 = true;
-    if (timer % 20 == 0) acc_t1 = true;
     if (timer % 20 == 0) acc_t3 = true;
-    if (timer % 20 == 0) acc_t2_a = true;
-    if (timer % 20 == 0) acc_t2_b = true;
-    if (timer >= 40) timer = 0;
+    if (timer % 20 == 0) acc_t1 = true;
+    if (timer % 40 == 0) {
+        acc_t2_a = true;
+        acc_t2_b = true;
+        acc_t2_c = true;
+        timer = 0;
+    }
     cyg_thread_resume(threadhndl1);
 }
 
 void cyg_user_start(void) {
 	// Initialize EZS counter
+    ezs_printf("start_application\n");
+
+    ezs_dac_init();
+
 	ezs_counter_init();
 
-	cyg_thread_create(1, &thread, 0, "lopp", my_stack, STACKSIZE,
+	cyg_clock_to_counter(cyg_real_time_clock(), &counter);
+
+	cyg_thread_create(1, &thread, 0, "loop", my_stack, STACKSIZE,
 	                  &threadhndl1, &threaddata);
-	cyg_alarm_create(counter, alarm_handler, NULL, &alarmhnd1, &alarm1);
+
+	cyg_alarm_create(counter, alarm_handler, 0, &alarmhnd1, &alarm1);
 	cyg_alarm_initialize(alarmhnd1, cyg_current_time() + 1, ms_to_cyg_ticks(1));
+    cyg_alarm_enable(alarmhnd1);
 }
+
