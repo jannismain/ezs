eb9b77f847a7570d1cc067ae25304e6f5b739a14
helene <helene.gsaenger@fau.de>
2017-01-17 18:21:41 +0100

    changed some code

--- exercise6/app.c
+++ exercise6/app.c
@@ -53,11 +53,16 @@
 #define TIME_DOMAIN_LENGTH (2 * PDS_LENGTH)
 static cyg_uint32 s_time_domain[TIME_DOMAIN_LENGTH];
 static float s_frequency_domain[PDS_LENGTH];
-static unsigned int s_position = 0;
+//static unsigned int s_position = 0;
+static unsigned int time_write = 0;
+static unsigned int time_read_3 = 0;
+static unsigned int time_read_4 = 0;
 
 #define SERIAL_BUFFER_LENGTH 15
+static volatile char c;
+static volatile bool c_empty = true;
 static char s_serial_buffer[SERIAL_BUFFER_LENGTH];
-static unsigned int s_serial_position;
+static unsigned int s_serial_position = 0;
 static bool s_command_decodable = false;
 
 enum CommandStatus
@@ -69,10 +74,16 @@ enum CommandStatus
 static enum CommandStatus packet_receive(char c)
 {
 	assert(s_serial_position < SERIAL_BUFFER_LENGTH);
-	/*
-	 * TODO: Code ergaenzen
-	 */
+    if (c == '\n') {
+        s_serial_buffer[s_serial_position] = '\0';
+        s_serial_position = 0;
+        c_empty = true;
+        return CommandComplete;
+    }
 
+    s_serial_buffer[s_serial_position++] = c;
+    c_empty = true;
+    return CommandIncomplete;
 }
 
 enum Command
@@ -96,42 +107,36 @@ enum State
 //Little helper functions.
 static cyg_tick_count_t ms_to_cyg_ticks(cyg_uint32 ms)
 {
-	/**
-	 * Implement if needed!
-	 **/
-	return ms;
+	cyg_handle_t clock = cyg_real_time_clock();
+	cyg_resolution_t res = cyg_clock_get_resolution(clock);
+	cyg_uint64 ticks = 1000000 * ms * (uint64_t)res.divisor / res.dividend;
+	return (cyg_tick_count_t)ticks;
 }
 
 static cyg_tick_count_t ms_to_ezs_ticks(cyg_uint32 ms) {
-	/**
-	 * Implement if needed!
-	 **/
-	return ms;
+	cyg_resolution_t res = ezs_counter_get_resolution();
+	cyg_uint64 ticks = 1000000 * ms * (uint64_t)res.divisor / res.dividend;
+	return (cyg_tick_count_t)ticks;
 }
 
 enum Command decode_command(void)
 {
+//    if (!strncmp(s_serial_buffer, "display ", 8));
 
 	enum Command ret = Invalid;
-
-	/*
-	 * TODO: Code ergaenzen
-	 */
 
 	return ret;
 }
 
 cyg_uint32 serial_isr_handler(cyg_vector_t vector, cyg_addrword_t data)
 {
-	/*
-	 * TODO: Code ergaenzen falls noetig
-	 */
+    if (!c_empty)
+        return CYG_ISR_HANDLED;
+
 	if (ezs_serial_char_available())
 	{
-		/*
-		 * TODO: Code ergaenzen
-		 */
-	        ezs_serial_getc();
+	    c = ezs_serial_getc();
+        c_empty = false;
 		cyg_interrupt_acknowledge(vector);
 		return CYG_ISR_CALL_DSR;
 	}
@@ -145,7 +150,7 @@ cyg_uint32 serial_isr_handler(cyg_vector_t vector, cyg_addrword_t data)
 
 void serial_dsr_handler(cyg_vector_t vec, cyg_ucount32 count, cyg_addrword_t data)
 {
-
+    packet_receive();
 	/*
 	 * TODO: Code ergaenzen
 	 */
@@ -161,9 +166,6 @@ static void polling_task_entry(cyg_addrword_t data)
 {
 	while (1)
 	{
-		/*
-		 * TODO: Code ergaenzen
-		 */
 		cyg_thread_suspend(cyg_thread_self());
 	}
 }
@@ -191,9 +193,9 @@ static void sampling_task_entry(cyg_addrword_t data)
 {
 	while (1)
 	{
-		/*
-		 * TODO: Code ergaenzen
-		 */
+        uint8_t x = ezs_adc_read();
+        s_time_domain[time_write++] = x;
+        time_write %= TIME_DOMAIN_LENGTH;
 
 		cyg_thread_suspend(cyg_thread_self());
 	}
@@ -207,15 +209,15 @@ static void procrastination_task_entry(cyg_addrword_t data)
 {
 	while (1)
 	{
-		/*
-		 * TODO: Code ergaenzen
-		 */
-
+        cyg_uint32 ticks = ms_to_ezs_ticks(10);
+        ezs_lose_time(ticks, 100);
 		cyg_thread_suspend(cyg_thread_self());
 	}
 }
 
 // T3
+static cyg_uint32 time_domain_copy[TIME_DOMAIN_LENGTH];
+
 static cyg_uint8     analysis_task_stack[STACKSIZE];
 static cyg_handle_t  analysis_task_handle;
 static cyg_thread    analysis_task_thread;
@@ -223,10 +225,13 @@ static void analysis_task_entry(cyg_addrword_t data)
 {
 	while (1)
 	{
-		/*
-		 * TODO: Code ergaenzen
-		 */
+        int idx;
+        for (idx = 0; idx < TIME_DOMAIN_LENGTH; idx++) {
+            time_domain_copy[idx] = s_time_domain[time_read_3++];
+            time_read_3 %= TIME_DOMAIN_LENGTH;
+        }
 
+        ezs_easy_pds(time_domain_copy, s_frequency_domain, TIME_DOMAIN_LENGTH);
 		cyg_thread_suspend(cyg_thread_self());
 	}
 }
@@ -240,10 +245,8 @@ static void display_signal_task_entry(cyg_addrword_t data)
 {
 	while (1)
 	{
-		/*
-		 * TODO: Code ergaenzen
-		 */
-
+//        ezs_plot(s_time_domain, TIME_DOMAIN_LENGTH, FB_BLACK, FB_WHITE); 
+//        ezs_lose_time(ms_to_ezs_ticks(500), 0);
 		cyg_thread_suspend(cyg_thread_self());
 	}
 }
@@ -256,9 +259,7 @@ static void display_pds_task_entry(cyg_addrword_t data)
 {
 	while (1)
 	{
-		/*
-		 * TODO: Code ergaenzen
-		 */
+        ezs_plot_pds(s_frequency_domain, PDS_LENGTH, FB_BLACK, FB_WHITE); 
 
 		cyg_thread_suspend(cyg_thread_self());
 	}
@@ -348,8 +349,8 @@ void cyg_user_start(void)
 	cyg_alarm_create(real_time_counter, procrastination_task_alarmfn, data_dummy, &procrastination_task_alarm_handle, &procrastination_task_alarm);
 	cyg_alarm_initialize(procrastination_task_alarm_handle, timebase + ms_to_cyg_ticks(PROCRASTINATION_TASK_PHASE), ms_to_cyg_ticks(PROCRASTINATION_TASK_PERIOD));
 
-	cyg_alarm_create(real_time_counter, analysis_task_alarmfn, data_dummy, &analysis_task_alarm_handle, &analysis_task_alarm);
-	cyg_alarm_initialize(analysis_task_alarm_handle, timebase + ms_to_cyg_ticks(ANALYSIS_TASK_PHASE), ms_to_cyg_ticks(ANALYSIS_TASK_PERIOD));
+    cyg_alarm_create(real_time_counter, analysis_task_alarmfn, data_dummy, &analysis_task_alarm_handle, &analysis_task_alarm);
+    cyg_alarm_initialize(analysis_task_alarm_handle, timebase + ms_to_cyg_ticks(ANALYSIS_TASK_PHASE), ms_to_cyg_ticks(ANALYSIS_TASK_PERIOD));
 
 	cyg_alarm_create(real_time_counter, display_signal_task_alarmfn, data_dummy, &display_signal_task_alarm_handle, &display_signal_task_alarm);
 	cyg_alarm_initialize(display_signal_task_alarm_handle, timebase + ms_to_cyg_ticks(DISPLAY_SIGNAL_TASK_PHASE), ms_to_cyg_ticks(DISPLAY_SIGNAL_TASK_PERIOD));
